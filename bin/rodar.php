5184626916ed6f49ef698b75ae820c8370c0d1ae
Thales-github <thalesvitoroliveiracirino@gmail.com>
2023-02-08 16:33:01 -0300

    socket

--- bin/rodar.php
+++ bin/rodar.php
@@ -3,20 +3,20 @@
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 $client = new WebSocket\Client("ws://localhost:789");
-$client->text('router abrindo o canal');
+$client->text('disparar');
 
 
-while (true) {
-    try {
-        $message = $client->receive();
-        if ($message) {
+// while (true) {
+//     try {
+//         $message = $client->receive();
+//         if ($message) {
 
-            print_r($message);
-            echo "\n";
-        }
-    } catch (\WebSocket\ConnectionException $e) {
-        // Possibly log errors
-        // print_r("Error: " . $e->getMessage());
-    }
-}
+//             print_r($message);
+//             echo "\n";
+//         }
+//     } catch (\WebSocket\ConnectionException $e) {
+//         // Possibly log errors
+//         // print_r("Error: " . $e->getMessage());
+//     }
+// }
 $client->close();
